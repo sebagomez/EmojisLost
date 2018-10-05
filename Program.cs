752fe7b64296ed7c69e9d72f60ed54339badb2a2
seba gomez <sebagomezcorrea@outlook.com>
2018-10-05 15:57:31 -0300

    simplest repo for bug(?) resolution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmojisLost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 😂");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EmojisLost
 repo for bug resolution
+
+Follow the conversation at: https://twitter.com/sebagomez/status/1047990503466315778
